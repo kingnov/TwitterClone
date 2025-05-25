068b105d91b1780320b3955ac8b020c6db005e97
KING DAVID <otwanemark254@gmail.com>
2025-05-25 21:26:09 +0300

    dom element

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// interfaces user, posts and comments 
+
+//select dom element from the html
+const pickUser = document.getElementById("pickUser") as HTMLSelectElement;
+
+//selectinguser 
+const userdeitails = document.getElementById("userdetail") as HTMLUListElement
+
+//post
+const userPost = document.getElementById("user-post") as HTMLUListElement;
+
+//comments
+const userComments = document.getElementById("user-comment") as HTMLUListElement;
+
+//userdetails call
